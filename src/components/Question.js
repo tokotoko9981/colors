800946b0fef79b4d7e3dbd94222d2c06193262ed
tokotoko9981 <tokotokotmy@gmail.com>
2025-02-20 02:26:58 +0900

    fix question image size

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,15 @@
+// src/app/components/Question.js
 import Image from "next/image";
 
 const Question = ({ image }) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex justify-center items-center w-full h-full max-h-[60vh]">
       <Image
         src={image}
         alt="問題の画像"
-        width={450}
-        height={300} 
-        className="rounded-lg"
+        width={300}
+        height={300}
+        className="object-contain h-full max-h-[60vh] w-auto"
       />
     </div>
   );
