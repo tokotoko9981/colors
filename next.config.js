aaceb3340518248d242480ba95ffa351c5cdf228
tokotoko9981 <tokotokotmy@gmail.com>
2025-02-20 07:13:53 +0900

    setup github pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/colors",
+    assetPrefix: "/colors/",
+  };
+  
+  module.exports = nextConfig;
+  
