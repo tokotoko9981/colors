dccd33efc8854c70e15279d5c0c642befd1449cd
tokotoko9981 <tokotokotmy@gmail.com>
2025-02-20 07:40:06 +0900

    fix github pages setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    images: {
+        unoptimized: true,
+    },
     basePath: "/colors",
     assetPrefix: "/colors/",
     trailingSlash: true,
